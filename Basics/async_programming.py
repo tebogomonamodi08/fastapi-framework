7fbfd29bf8d52bf35fae4b852551a4acfd8faa94
tebogomonamodi08 <150713484+tebogomonamodi08@users.noreply.github.com>
2025-04-12 23:05:33 +0200

    more asynchrounous programming

--- Basics/async_programming.py
+++ Basics/async_programming.py
@@ -1,5 +1,6 @@
-import asyncio
 
+import asyncio
+"""
 
 async def task(t : int, n: int):
     '''definition of a asynchronous function that take parameter t as time and n as the task number and yields
@@ -14,6 +15,25 @@ async def main():
     task_1 = task(4,1)
     task_2 = task(2,2)
     await asyncio.gather(task_1,task_2)
+
+
+asyncio.run(main())"""
+
+async def task(t : float, n : int):
+    '''This is a asynchronous function that simulates fetching data'''
+    print('Fetching data...')
+    await asyncio.sleep(t) #simulate the time in t seconds
+    if n==2:
+        raise ValueError('Eish')
+    return f'Data from task {n}' #return this when the wait is finished
+
+async def main():
+    task_1 = task(4,1)
+    task_2 = task(5,2)
+    task_3 = task(3, 3)
+
+    results = await asyncio.gather(task_1,task_2,task_3, return_exceptions=True)
+    print(results)
 
 
 asyncio.run(main())
